f3dc4613bd9e4bd498e20be9bb575a2a721aeaae
vdud <varundudeja96@gmail.com>
2023-03-05 11:27:45 +0530

    public-functionality

--- src/routes/[[user]]/[PUB]/+page.server.ts
+++ src/routes/[[user]]/[PUB]/+page.server.ts
@@ -22,12 +22,75 @@ export const load = (async ({ params }) => {
 			})
 		} else {
 			const findGroup = await groups.findOne({ name: `${userSender.name};${userReciever.name}` })
+			const findSecondGroup = await groups.findOne({ name: `${userReciever.name};${userSender.name}` })
 
-			if (!findGroup) {
+			if (!findGroup && !findSecondGroup) {
 				await groups.insertOne({ name: `${userSender.name};${userReciever.name}`, allUsers: [userSender._id, userReciever._id], nature: 'PUBLIC', createdAt: new Date(), updatedAt: new Date() }).then(async (res) => {
 					await mainUser.updateOne({ _id: userSender._id }, { $push: { allGroups: res.insertedId } })
 					await mainUser.updateOne({ _id: userReciever._id }, { $push: { allGroups: res.insertedId } })
 				})
+			}
+		}
+		const userRecieverAgain = await mainUser.findOne({ name: PUB })
+
+		if (userRecieverAgain) {
+			const returnData = await groups
+				.aggregate([
+					{ $match: { name: `${userRecieverAgain.name};${userSender.name}` } },
+					{
+						$lookup: {
+							from: 'user',
+							localField: 'allUsers',
+							foreignField: '_id',
+							as: 'allUsers',
+						},
+					},
+					{
+						$project: {
+							_id: 0,
+							name: 1,
+							messages: 1,
+							allUsers: {
+								_id: 1,
+								name: 1,
+							},
+						},
+					},
+				])
+				.toArray()
+
+			if (returnData.length === 0) {
+				const returnData = await groups
+					.aggregate([
+						{ $match: { name: `${userSender.name};${userRecieverAgain.name}` } },
+						{
+							$lookup: {
+								from: 'user',
+								localField: 'allUsers',
+								foreignField: '_id',
+								as: 'allUsers',
+							},
+						},
+						{
+							$project: {
+								_id: 0,
+								name: 1,
+								messages: 1,
+								allUsers: {
+									_id: 1,
+									name: 1,
+								},
+							},
+						},
+					])
+					.toArray()
+			}
+
+			return {
+				status: 200,
+				body: {
+					data: JSON.stringify(returnData[0]),
+				},
 			}
 		}
 	}
